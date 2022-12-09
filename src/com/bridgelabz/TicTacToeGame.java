1b4c6ca3744059605e9ac3083fd042720aa08565
akashp69 <akashpakhare499@gmail.com>
2022-12-10 00:56:15 +0530

    Added: Program For Choose the letter for  Player

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,6 +1,10 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+    static char playerLetter;
+    static char computerLetter;
     /*
     *This is static Method Used for create empty  tic-tac-toe board
     */
@@ -11,6 +15,21 @@ public class TicTacToeGame {
             System.out.print(board[i] + " ");
         }
     }
+    /*
+     * This Is static method is used for chose the letter
+     */
+    public static void chooseLetter() {
+
+        System.out.println("\nChoose your Letter Either X or O");
+        Scanner scanner = new Scanner(System.in);
+        playerLetter = scanner.next().toUpperCase().charAt(0);
+        if (playerLetter == 'X'){
+            System.out.println(playerLetter);
+        }
+        else {
+            System.out.println( computerLetter==0);
+        }
+    }
 
     /**
      *This is Main Method is Used for calling the static method gameBoard
@@ -18,5 +37,6 @@ public class TicTacToeGame {
     public static void main(String[] args) {
         System.out.println("Welcome To Tic-Tac-Toe Game");
         gameBoard();
+        chooseLetter();
     }
 }
